58ef1ea2c0f30bbe49b0d869c6d5be33061b313b
Jozef Zigmund <jzigmund@redhat.com>
2017-11-09 18:15:58 +0100

    trying install NPM

--- update_documentation.sh
+++ update_documentation.sh
@@ -2,10 +2,10 @@
 
 request_update_doc() {
 	echo "Init update doc"
-	git init
-	git config user.name "Ghost"
 	git clone https://github.com/jzigmund/jzigmund.github.io.git
 	cd jzigmund.github.io
+	git init
+	git config user.name "Ghost"
 	echo "$(pwd)"
 	git checkout master
 	echo "GIT STATUS: $(git status)"
@@ -15,16 +15,26 @@ request_update_doc() {
 	git push -q origin HEAD:master
 }
 
-echo "TRIGGER DOCS"
-changed_files=`git diff --name-only HEAD^`
-echo "$changed_files"
+install_npm_deps() {
+	echo "install nodejs"
+	sudo apt-get install -y nodejs
+	echo "NPM $(npm -v)"
+}
 
-if [[ $changed_files =~ .*api.yaml ]]; then
-	echo "It's there"
-	request_update_doc
+#checks if it's merge action
+if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master"]]; then
+	echo "Update documentation has been triggered"
+	changed_files=`git diff --name-only HEAD^`
+	echo "$changed_files"
+	# checks if merged PR contains any changes in api.yaml
+	if [[ $changed_files =~ .*api.yaml ]]; then
+		install_npm_deps
+		echo "It's there"
+		#request_update_doc
+	else
+		echo "Not there"
+		exit 0
+	fi
 else
-	echo "Not there"
-	exit 0
+	echo "Update documentation has not been triggered"
 fi
-
-
