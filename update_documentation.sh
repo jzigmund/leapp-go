6a21699314ef857e74cf3f3dbced685e55236d3e
Jozef Zigmund <jzigmund@redhat.com>
2017-11-14 16:52:43 +0100

    push test

--- update_documentation.sh
+++ update_documentation.sh
@@ -15,7 +15,6 @@ request_update_doc() {
 	echo "GIT STATUS: $(git status)"
 	echo "REV $commit"
 	git commit -m "rebuild pages at $commit"
-	echo "GIT SHOW $(git show)"
 	git push origin HEAD:master
 }
 
