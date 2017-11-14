1ad7a4f6f3b751a86f526693649cd92cfb91b412
Jozef Zigmund <jzigmund@redhat.com>
2017-11-14 15:42:36 +0100

    push test

--- update_documentation.sh
+++ update_documentation.sh
@@ -2,16 +2,16 @@
 
 request_update_doc() {
 	echo "Init update doc"
-	git clone https://github.com/jzigmund/jzigmund.github.io.git
+	git clone "https://$GH_TOKEN@github.com/jzigmund/jzigmund.github.io.git"
 	cd jzigmund.github.io
 	git init
 	git config user.name "Ghost"
 	echo "$(pwd)"
 	git checkout master
 	echo "GIT STATUS: $(git status)"
-	touch index.md
-	git add -A .
-	git commit -m "rebuild pages"
+	cp ../test.md shins/source/index.html.md
+	git add -A shins/source/index.html.md
+	git commit -m "rebuild pages at $(rev)"
 	git push -q origin HEAD:master
 }
 
@@ -22,20 +22,21 @@ install_npm_deps() {
 
 convert_documentation() {
 	node /home/travis/.nvm/versions/node/v7.4.0/bin/widdershins -y ./docs/api/api.yaml -o test.md
-	cat test.md
+	echo "NVM - $(NVM_PATH) \nNODE - $(NODE_PATH)"
 }
 
 #checks if it's merge action
 if [[ $TRAVIS_PULL_REQUEST == "false" && $TRAVIS_BRANCH == "master" ]]; then
 	echo "Update documentation has been triggered"
 	changed_files=`git diff --name-only HEAD^`
+	rev=$(git rev-parse --short HEAD)
 	echo "$changed_files"
 	# checks if merged PR contains any changes in api.yaml
 	if [[ $changed_files =~ .*api.yaml ]]; then
 		install_npm_deps
 		convert_documentation
 		echo "It's there"
-		#request_update_doc
+		request_update_doc
 	else
 		echo "Not there"
 		exit 0
